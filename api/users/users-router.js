7c62059b779b7ca0b7988e606d3720be16e283cd
Giovanni  Rosales <giovannirosales@Giovannis-MacBook-Pro.local>
2021-12-01 22:20:13 -0600

    finished

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -36,35 +36,60 @@ router.post('/', validateUser, (req, res, next) => {
   .catch(next)
 });
 
-router.put('/:id', validateUserId, validateUser, (req, res) => {
+router.put('/:id', validateUserId, validateUser, (req, res, next) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
-  console.log(req.user)
-  console.log(req.name)
+ User.update(req.params.id, { name: req.name})
+ .then(() => {
+   return User.getById(req.params.id)
+ })
+ .then( user => {
+   res.json(user)
+ })
+ .catch(next)
 });
 
-router.delete('/:id', validateUserId, (req, res) => {
+router.delete('/:id', validateUserId, async (req, res, next) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
-  console.log(req.user)
+      try {
+        await User.remove(req.params.id)
+        res.json(req.user)
+      } catch (err) {
+        next(err)
+      }
 });
 
-router.get('/:id/posts', validateUserId, (req, res) => {
+router.get('/:id/posts', validateUserId, async (req, res, next) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
-  console.log(req.user)
+      try {
+        const result = await User.getUserPosts(req.params.id)
+        res.json(result)
+      } catch (err) {
+        next(err)
+      }
 });
 
-router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
+router.post('/:id/posts', 
+validateUserId, 
+validatePost, async (req, res, next) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
-  console.log(req.user)
-  console.log(req.text)
+      try {
+        const result = await Post.insert({
+          user_id:req.params.id,
+          text: req.text,
+        })
+        res.status(201).json(result)
+      } catch (err) {
+        next(err)
+      }
 });
 
-router.use((err, req, res, next) => {
+router.use((err, req, res, next) => { //eslint-disable-line
   res.status(err.status || 500).json({
     customMessage: 'something tragic inside posts router happened',
     message: err.message,
